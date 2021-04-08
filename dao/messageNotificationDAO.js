54786e9dd54fb1d4b83e27a7ff38d4a7c289b7d2
Ons Ben Slimen <ons.benslimen@esprit.tn>
2021-04-08 21:01:40 +0100

    .

--- dao/messageNotificationDAO.js
+++ dao/messageNotificationDAO.js
@@ -2,15 +2,16 @@ var messageNotificationDAO = module.exports = function (models) {
     this.models = models;
 };
 
-messageNotificationDAO.prototype.createNotification = (notification, cb)=>{
+messageNotificationDAO.prototype.createNotification = function(notification, cb){
     var newNotification = {
         "id": notification.id,
         "companyId": notification.companyId,
         "candidateId": notification.candidateId ,
         "receiver": notification.receiver
     };
-
+    console.log("In here");
     this.models.message_notifications.create(newNotification).then(createdNotification => {
+
         if (!createdNotification)
             return cb(Error("notification not created !"));
         else
